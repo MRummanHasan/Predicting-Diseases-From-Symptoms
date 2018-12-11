c162a2b6b0db0eda4e9bc4408f20542bbd73a0a0
Muhammad Rumman Hasan Khan <mrummanhasan@gmail.com>
2018-12-11 23:36:35 +0500

    prediction for more then one symptom

--- disPred.py
+++ disPred.py
@@ -1,4 +1,4 @@
-######################## # # #Cleaning our data# # # #####################
+    ######################## # # #Cleaning our data# # # #####################
 import pandas as pd
 import csv
 from collections import defaultdict
@@ -45,16 +45,17 @@ with open("Scraped-Data/dataset_clean.csv","w") as csvfile:
         for v in values:
             #key = str.encode(key)
             key = str.encode(key).decode('utf-8')
-            #.strip()
+            '''#.strip()
             #v = v.encode('utf-8').strip()
-            #v = str.encode(v)
+            #v = str.encode(v)'''
             writer.writerow([key,v,dict_wt[key]])
 
 columns = ['Source','Target','Weight']
 data = pd.read_csv("Scraped-Data/dataset_clean.csv",names=columns, encoding ="ISO-8859-1")
 
 
-data.to_csv("Scraped-Data/dataset_clean.csv",index=False)
+#separating disease an symtpoms in separate files
+data.to_csv("Scraped-Data/dataset_clean.csv",index=False) #Source/disease,Target/symp,Weight
 slist = []
 dlist = []
 with open("Scraped-Data/nodetable.csv","w") as csvfile:
@@ -78,9 +79,6 @@ nt_data.to_csv("Scraped-Data/nodetable.csv",index=False)
 
 
 
-
-
-
 ####################### # # #Analysing our cleaned data# # # #########################
 data = pd.read_csv("Scraped-Data/dataset_clean.csv", encoding ="ISO-8859-1")
 # print(len(data['Source'].unique()))
@@ -99,12 +97,11 @@ df_pivoted = df_pivoted.groupby('Source').sum()
 df_pivoted = df_pivoted.reset_index()
 # print(df_pivoted[:5])
 
-
 df_pivoted.to_csv("Scraped-Data/df_pivoted.csv")
 x = df_pivoted[cols]
 y = df_pivoted['Source']
-print(x)
-print(y)
+# print(x)
+# print(y)
 
 # # Trying out our classifier to learn diseases from the symptoms
 import seaborn as sns
@@ -115,11 +112,8 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 mnb = MultinomialNB()
 mnb = mnb.fit(x_train, y_train)
-mnb.score(x_test, y_test)
-print()
-
-
-
+print(mnb.score(x_test, y_test))
+# print(x_train, x_test, y_train, y_test)
 
 
 
@@ -130,11 +124,12 @@ mnb_tot.score(x, y)
 
 disease_pred = mnb_tot.predict(x)
 disease_real = y.values
+print(y.values)
 for i in range(0, len(disease_real)):
     if disease_pred[i]!=disease_real[i]:
         print ('Pred: {0} Actual:{1}'.format(disease_pred[i], disease_real[i]))
-
-
+    else:
+        print ("xxxxxxxxxxxxxx",'Pred: {0} Actual:{1}'.format(disease_pred[i], disease_real[i]))
 
 
 
@@ -143,9 +138,8 @@ for i in range(0, len(disease_real)):
 from sklearn.tree import DecisionTreeClassifier
 print ("DecisionTree")
 dt = DecisionTreeClassifier()
-clf_dt=dt.fit(x,y)
+clf_dt = dt.fit(x,y)
 print ("Acurracy: ", clf_dt.score(x,y))
-
 
 
 
@@ -154,44 +148,12 @@ print ("Acurracy: ", clf_dt.score(x,y))
 from sklearn import tree 
 ####################### # # # Analysis of the Manual data # # # ######################################
 data = pd.read_csv("Manual-Data/Training.csv")
-#  # data.columns
 df = pd.DataFrame(data)
 # The manual data contains approximately 4920 rows.
 cols = df.columns
 cols = cols[:-1]
 x = df[cols]
 y = df['prognosis']
-
-
-
-
-
-######################## #Trying out our classifier to learn diseases from the symptoms #######################
-import seaborn as sns
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-mnb = MultinomialNB()
-mnb = mnb.fit(x_train, y_train)
-# print(mnb.score(x_test, y_test))
-
-'''
-from sklearn import model_selection  #changed from=> import cross_validation
-print ("cross result========")
-scores = model_selection.cross_val_score(mnb, x_test, y_test, cv=3)
-print (scores)
-print (scores.mean())
-# We use the testing dataset to actually test our Multinomial Bayes model
-'''
-test_data = pd.read_csv("Manual-Data/Testing.csv")
-# print(test_data.head())
-
-testx = test_data[cols]
-testy = test_data['prognosis']
-mnb.score(testx, testy)
-
-
-
 
 
 
@@ -203,15 +165,7 @@ dt = DecisionTreeClassifier()
 clf_dt=dt.fit(x_train,y_train)
 print ("Acurracy: ", clf_dt.score(x_test,y_test))
 
-'''
-from sklearn import model_selection  #changed from=> import cross_validation
-print ("cross result========")
-scores = model_selection.cross_val_score(mnb, x_test, y_test, cv=3)
-print (scores)
-print (scores.mean())
-'''
 
-print ("Acurracy on the actual test data: ", clf_dt.score(testx,testy))
 # dt.__getstate__()
 
 #Finding the Feature importances
@@ -222,26 +176,38 @@ indices = np.argsort(importances)[::-1]
 # # Print the feature ranking
 print("Feature ranking:")
 features = cols
-for f in range(10):
+for f in range(132):
     print("%d. feature %d - %s (%f)" % (f + 1, indices[f], features[indices[f]] ,importances[indices[f]]))
 
 feature_dict = {}
 for i,f in enumerate(features):
     feature_dict[f] = i
-print(feature_dict['stomach_pain'])
-print(feature_dict['redness_of_eyes'])
-s1 = feature_dict['stomach_pain']
-s2 = feature_dict['redness_of_eyes']
+
+print(features)
+
+s1 = feature_dict['itching']
+s2 = feature_dict['joint_pain']
+# s3 = feature_dict['stomach_pain']
+
+sample_a = [1 if i == int(s1) else 0 for i in range(len(features))]
+# sample_b = [i/int(s2) if i == int(s2) else i*0 for i in range(len(features))]
+#or
+sample_b = []
+for i in range(len(features)):
+    if i == int(s2):
+       sample_b.append(1)
+    else:
+        sample_b.append(0)
 
 
-sample_b = [i/int(s1) if i == int(s1) else i*0 for i in range(len(features))]
-sample_a = [i/int(s2) if i == int(s2) else i*0 for i in range(len(features))]
 a = np.matrix(sample_a)
 b = np.matrix(sample_b)
 bb = a + b
+print(len(a),len(b),len(bb))
 sample_b = np.array(sample_b).reshape(1,len(sample_b))
 sample_a = np.array(sample_a).reshape(1,len(sample_a))
-# print(sample_b,sample_a)
+
+print(bb)
 
 print(dt.predict(bb))
 # print(dt.predict_proba(bb))
